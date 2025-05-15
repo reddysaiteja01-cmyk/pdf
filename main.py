b157c8c65774eb8eb1ca30d993159a9f73c5d446
pdfqrscan <tejasaik89@gmail.com>
2025-05-15 18:34:13 +0530

    Update main.py

--- main.py
+++ main.py
@@ -6,12 +6,12 @@ app = FastAPI()
 
 @app.get("/download")
 def download_pdf():
-    file_path = "files/your_file.pdf"
+    file_path = "files/Occupancy_Certificate.pdf"
     if os.path.exists(file_path):
         return FileResponse(
             path=file_path,
-            filename="your_file.pdf",
+            filename="Occupancy_Certificate.pdf",
             media_type="application/pdf",
-            headers={"Content-Disposition": "attachment; filename=your_file.pdf"}
+            headers={"Content-Disposition": "attachment; filename=Occupancy_Certificate.pdf"}
         )
     return Response(content="File not found", status_code=404)
